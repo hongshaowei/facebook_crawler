27d0521c642a4c6ef3b6a5432b9a0c8c85b39860
david <davidtnfsh@gmail.com>
2016-09-13 14:03:36 +0800

    修改一個字

--- csv.py
+++ csv.py
@@ -1,6 +1,6 @@
 import json, pandas
 def write_csv():
-	with open('untitled.json', 'w') as outf:
+	with open('untitled.json', 'r') as outf:
 		j = json.load(outf) 
 		df = pandas.DataFrame(j)
 		df.to_csv('result.csv')
